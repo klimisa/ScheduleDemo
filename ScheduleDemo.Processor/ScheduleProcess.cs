8f85872ad7ab78164cf1b4ee40a44e999055d823
Antonios Klimis <klimis@intralot.com>
2016-02-16 19:31:15 +0200

    First commit

---
+++ ScheduleDemo.Processor/ScheduleProcess.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScheduleDemo.Processor
+{
+    public class ScheduleProcess
+    {
+        public bool Enabled { get; set; }
+
+        public void Start()
+        {
+//            var producer = new ProduceDelegate(Poll);
+//            producer.Invoke();
+            var t = new Thread(Poll);
+            t.Start();
+        }
+
+        private void Poll()
+        {
+            var counter = 0;
+
+            while (true)
+            {
+                if (!Enabled)
+                    break;
+
+                var count = counter.ToString(CultureInfo.InvariantCulture).PadLeft(6, '0');
+                Console.WriteLine("Started {0}", count);
+                counter++;
+
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Inactive");
+        }
+
+
+        private delegate void ProduceDelegate();
+    }
+}
---
+++ ScheduleDemo.Terminal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScheduleDemo.Processor;
+
+namespace ScheduleDemo.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var process = new ScheduleProcess
+            {
+                Enabled = true
+            };
+
+            while (true)
+            {
+                var keyPressed = Console.ReadKey();
+                if (keyPressed.Key == ConsoleKey.Q)
+                {
+                    process.Enabled = false;
+                }
+
+                if (keyPressed.Key == ConsoleKey.Enter)
+                {
+                    process.Enabled = true;
+                    process.Start();
+                }
+            }
+        }
+    }
+}
